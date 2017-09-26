8b071f696847db367766d8f61b82b7697cb84642
sunleic <sunleic@yonyou.com>
2017-09-26 19:47:38 +0800

    thread base learning status and notify date:2017-09-26

---
+++ src/com/sun/test/thread04/Deprecated.java
@@ -0,0 +1,44 @@
+package com.sun.test.thread04;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by sunleic on 2017/9/26.
+ * Version by ${VERSION}
+ */
+public class Deprecated {
+    public static void main(String[] args) throws InterruptedException {
+        DateFormat format = new SimpleDateFormat("HH:mm:ss");
+        Thread printThread = new Thread(new Runner(), "PrintThread");
+        printThread.setDaemon(true);
+        printThread.start();
+        TimeUnit.SECONDS.sleep(3);
+        //将printThread进行暂停
+        printThread.suspend();
+        System.out.println("main suspend PrintThread at " + format.format(new Date()));
+        TimeUnit.SECONDS.sleep(3);
+        //将将printThread进行恢复
+        printThread.resume();
+        System.out.println("main resume PrintThread at " + format.format(new Date()));
+        TimeUnit.SECONDS.sleep(3);
+        //将printThread进行停止
+        printThread.stop();
+        System.out.println("main stop PrintThread at " + format.format(new Date()));
+        TimeUnit.SECONDS.sleep(3);
+
+    }
+
+    static class Runner implements Runnable {
+        @Override
+        public void run() {
+            DateFormat format = new SimpleDateFormat("HH:mm:ss");
+            while (true) {
+                System.out.println(Thread.currentThread().getName() + " Run at " + format.format(new Date()));
+                SleepUtils.second(1);
+            }
+        }
+    }
+}
---
+++ src/com/sun/test/thread04/Priority.java
@@ -0,0 +1,57 @@
+package com.sun.test.thread04;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程优先级
+ * Created by sunleic on 2017/9/26.
+ * Version by ${VERSION}
+ */
+public class Priority {
+    private static volatile boolean notState = true;
+    private static volatile boolean notEnd = true;
+
+    public static void main(String[] args) throws Exception {
+        List<Job> jobslist = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int priority = i < 5 ? Thread.MIN_PRIORITY : Thread.MAX_PRIORITY;
+            Job job = new Job(priority);
+            jobslist.add(job);
+            Thread thread = new Thread(job, "Thread:" + i);
+            thread.setPriority(priority);
+            thread.start();
+        }
+
+        notState = false;
+        TimeUnit.SECONDS.sleep(10);
+        notEnd = false;
+
+        for (Job job : jobslist) {
+            System.out.println("Job Priority : " + job.priority + ",Count:" + job.jobCount);
+        }
+
+    }
+
+    static class Job implements Runnable {
+
+        private int priority;
+        private long jobCount;
+
+        public Job(int priority) {
+            this.priority = priority;
+        }
+
+        @Override
+        public void run() {
+            while (notState) {
+                Thread.yield();
+            }
+            while (notEnd) {
+                Thread.yield();
+                jobCount++;
+            }
+        }
+    }
+}
