68f5f9f129c43b773581eab290080ae41bc42f1a
sunleic <sunleic@yonyou.com>
2017-09-27 16:29:32 +0800

    threadPool implements method one 2017-09-27

---
+++ src/com/sun/test/thread04/ThreadPool.java
@@ -0,0 +1,23 @@
+package com.sun.test.thread04;
+
+/**
+ * 线程池接口定义
+ * Created by sunleic on 2017/9/27.
+ * Version by ${VERSION}
+ */
+public interface ThreadPool<Job extends Runnable> {
+    //执行一个job，这个job需要实现Runnable
+    void execute(Job job);
+
+    //关闭线程池
+    void shutdown();
+
+    //增加工作者线程
+    void addWorkers(int num);
+
+    //减少工作者
+    void removeWorker(int num);
+
+    //得到正在等待执行的任务数量
+    int getJobSize();
+}
